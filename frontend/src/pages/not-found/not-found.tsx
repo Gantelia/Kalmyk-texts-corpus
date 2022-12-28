6647c0bacdbc8d4468a29ab147d56354cb6a076e
Gantelia <helengriva@gmail.com>
2022-12-28 23:56:13 +0300

    Изменит подключение стилей в <About/> и <Not-Found/>

--- frontend/src/pages/not-found/not-found.tsx
+++ frontend/src/pages/not-found/not-found.tsx
@@ -1,8 +1,9 @@
 import './not-found.scss';
+import '../../sass/common/main.scss';
 
 function NotFound() {
   return (
-    <main className="main not-found__main">
+    <main className="main">
       <h1 className="not-found__title">404</h1>
       <p className="not-found__text">Страница не найдена</p>
     </main>
